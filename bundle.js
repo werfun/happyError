298a2c7fa5aff502a14c3dbdf765cabda52a58d7
HaPi <maoxiaobo@foxmail.com>
2019-07-08 20:13:05 +0800

    init

---
+++ bundle.js
@@ -0,0 +1,34 @@
+;(function () {
+  function ajaxEventTrigger(event) {
+  //  var ajaxEvent = new CustomEvent(event, { detail: this });
+  //  window.dispatchEvent(ajaxEvent);
+   console.log('ajaxEvent', event)
+  }
+     
+  var oldXHR = window.XMLHttpRequest;
+    
+  function newXHR() {
+   var realXHR = new oldXHR();
+    
+  //  realXHR.addEventListener('abort', function (ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+  //  realXHR.addEventListener('error', function (ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+  //  realXHR.addEventListener('load', function (ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+   realXHR.addEventListener('loadstart', function (ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+  //  realXHR.addEventListener('progress', function (ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+  //  realXHR.addEventListener('timeout', function (ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+  //  realXHR.addEventListener('loadend', function (ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+  //  realXHR.addEventListener('readystatechange', function(ev) { ajaxEventTrigger.call(this, ev); }, false);
+    
+   return realXHR;
+  }
+    
+  window.XMLHttpRequest = newXHR;
+ })();
+
---
+++ error.js
@@ -0,0 +1,22 @@
+window.onerror = function (msg, url, row, col, error) {
+  console.log('我知道错误了');
+  console.log({
+    msg,  url,  row, col, error
+  })
+  return true;
+};
+
+window.addEventListener('error', (msg, url, row, col, error) => {
+  console.log('我知道 404 错误了');
+  console.log(
+    msg, url, row, col, error
+  );
+  return true;
+}, true);
+
+window.addEventListener("unhandledrejection", function(e){
+  e.preventDefault()
+  console.log('我知道 promise 的错误了');
+  console.log(e.reason);
+  return true;
+});
